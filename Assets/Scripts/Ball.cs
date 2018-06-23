4e98e718c4a8cd89c5b6186b5f8c703ec683e31e
Mark Carroll <markjcarroll@hotmail.co.uk>
2018-06-23 11:56:40 +0100

    Primary commit...

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+    public Paddle paddle;
+    private Rigidbody2D rigi;
+    private Vector3 paddleToBallVector;
+    private bool hasStarted = false;
+    private void Awake()
+    {
+        rigi = GetComponent<Rigidbody2D>();
+    }
+    // Use this for initialization
+    void Start () {
+        paddleToBallVector = this.transform.position - paddle.transform.position;
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!hasStarted)
+        {
+            // lock ball to paddle
+            this.transform.position = paddle.transform.position + paddleToBallVector;
+            if (Input.GetMouseButtonDown(0))
+            {
+                print("Mouse Clicked!");
+                hasStarted = true;
+                rigi.velocity = new Vector2(2f, 10f);
+            }
+        }
+	}
+}
